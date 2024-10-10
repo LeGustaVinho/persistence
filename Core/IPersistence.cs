cbaf0f07aa2bbe8c6c02491ceab545fe770ed9a3
Gustavo <gustavo.oliveaxe@gmail.com>
2024-10-10 10:46:41 -0300

    IPersistence now implements IDisposable

---
+++ Core/IPersistence.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LegendaryTools.Persistence
+{
+    public interface IPersistence : IDisposable
+    {
+        public bool IsBusy { get; }
+        string Set<T>(T dataToSave, string id = Persistence.EMPTY_ID, int version = 0);
+        T Get<T>(string id);
+        (int, int, DateTime) GetMetadata<T>();
+        Dictionary<string, T> GetCollection<T>();
+        bool Delete<T>(string id);
+        bool Contains<T>(string id);
+        void Save();
+        void Load();
+        Task SaveAsync();
+        Task LoadAsync();
+        void AddListener<T>(Action<IPersistence, PersistenceAction, string, object> callback);
+        void RemoveListener<T>(Action<IPersistence, PersistenceAction, string, object> callback);
+    }
+}
--- Core/Persistence.cs
+++ Core/Persistence.cs
@@ -6,30 +6,6 @@ using UnityEngine;
 
 namespace LegendaryTools.Persistence
 {
-    public interface IPersistence
-    {
-        public bool IsBusy { get; }
-        string Set<T>(T dataToSave, string id = Persistence.EMPTY_ID, int version = 0);
-        T Get<T>(string id);
-        (int, int, DateTime) GetMetadata<T>();
-        Dictionary<string, T> GetCollection<T>();
-        bool Delete<T>(string id);
-        bool Contains<T>(string id);
-        void Save();
-        void Load();
-        Task SaveAsync();
-        Task LoadAsync();
-        void AddListener<T>(Action<IPersistence, PersistenceAction, string, object> callback);
-        void RemoveListener<T>(Action<IPersistence, PersistenceAction, string, object> callback);
-    }
-
-    public enum PersistenceAction
-    {
-        Add,
-        Update,
-        Delete,
-    }
-    
     public class Persistence : IPersistence
     {
 #if ODIN_INSPECTOR
@@ -443,6 +419,11 @@ namespace LegendaryTools.Persistence
                         persistenceCallback.OnAfterDeserialize();
                 }
             }
+        }
+
+        public void Dispose()
+        {
+            DataTables.Clear();
         }
     }
 }
